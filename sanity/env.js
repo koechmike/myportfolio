67109f7006d9d3d34b5fb3f488a21bbf8241e752
Mike koech <140485926+miketracking@users.noreply.github.com>
2024-02-27 23:30:03 +0300

    env changes

--- sanity/env.js
+++ sanity/env.js
@@ -2,11 +2,7 @@ export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-02
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "r7jungte";
 export const useCdn = false
-<<<<<<< HEAD
-export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3005";
-=======
 export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001";
->>>>>>> ebf1c34b7e70d8b5ffc77d388783a27399a5fc26
 
 // import { config } from 'dotenv'
 
