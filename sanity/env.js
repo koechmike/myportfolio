b477ae1074b89eea88c0bfc7dbbc042bb816d1d6
Mike koech <140485926+miketracking@users.noreply.github.com>
2024-03-05 07:13:39 +0300

    wgsafsfas

--- sanity/env.js
+++ sanity/env.js
@@ -2,7 +2,7 @@ export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-02
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "r7jungte";
 export const useCdn = false
-export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001";
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3005";
 
 // import { config } from 'dotenv'
 
